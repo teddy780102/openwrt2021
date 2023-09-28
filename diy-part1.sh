91ae5b43beafb969c4d332bd1ea8c936b755887b
teddy780102 <70836303+teddy780102@users.noreply.github.com>
2023-09-28 11:47:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #echo 'src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git golang https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang' >>feeds.conf.default
