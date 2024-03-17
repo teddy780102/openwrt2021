0ff75cdf1328aa3313e92876239cdc2b0e6721d3
teddy780102 <70836303+teddy780102@users.noreply.github.com>
2024-03-17 16:03:14 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+./scripts/feeds clean
+./scripts/feeds update -a
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a
