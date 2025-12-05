df2ae7242f8e6fd5d9b6bde24d93f7974d402819
teddy780102 <70836303+teddy780102@users.noreply.github.com>
2025-12-05 13:11:14 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,5 @@
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/luci2/bin/config_generate
 # sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/' feeds/packages/utils/xfsprogs/Makefile
-rm -rf  feeds/small8/luci-app-lucky
-rm -rf  feeds/small8/lucky
-git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+#rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
