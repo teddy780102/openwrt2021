2b6d28fcb90fade07d0f38d520089c4eea11a6f1
teddy780102 <70836303+teddy780102@users.noreply.github.com>
2025-12-05 13:09:37 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -12,5 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+rm -rf  feeds/small8/luci-app-lucky
+rm -rf  feeds/small8/lucky
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
